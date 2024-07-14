f05259b8e0a0818c828426aa9ebd355fda107d4e
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-14 16:48:51 +0530

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,18 +1,16 @@
-def ks(val, W, wt, n):
-    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+def knapsack(val, weight, wt, n):
+    K = [[0 for x in range(weight + 1)] for x in range(n + 1)]
 
     for i in range(n + 1):
-        for w in range(W + 1):
+        for w in range(weight + 1):
             if i == 0 or w == 0:
                 K[i][w] = 0
             elif wt[i-1] <= w:
-                K[i][w] = max(val[i-1]
-                              + K[i-1][w-wt[i-1]],
-                              K[i-1][w])
+                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]] , K[i-1][w])
             else:
                 K[i][w] = K[i-1][w]
 
-    return K[n][W]
+    return K[n][weight]
 
 
 n = int(input("Enter the number of values: "))
