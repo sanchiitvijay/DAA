26bdb091fa0f6f244cfd8071a62bfac1de72d502
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-14 16:14:34 +0530

    Update belllman-ford.py

--- belllman-ford.py
+++ belllman-ford.py
@@ -1,18 +1,9 @@
-
-
-
-def addEdge(u, v, w, graph):
-    graph.append([u, v, w])
-
-
 def printArr(graph, V, dist):
     print("Vertex Distance from Source")
     for i in range(V):
         print("{0}\t\t{1}".format(i, dist[i]))
 
-
 def BellmanFord(graph, V, src):
-
     dist = [float("Inf")] * V
     dist[src] = 0
 
@@ -32,9 +23,11 @@ num_vertices = int(input("Enter the number of vertices: "))
 num_edges = int(input("Enter the number of edges: "))
 graph = []
 V = num_vertices
+
 print("Enter edges as 'u v w', where u = source, v = destination, w = weight:")
 for _ in range(num_edges):
     u, v, w = map(int, input().split())
-    addEdge(u, v, w, graph)
+    graph.append([u, v, w])
 source_vertex = int(input("Enter the source vertex: "))
+
 BellmanFord(graph, V, source_vertex)
